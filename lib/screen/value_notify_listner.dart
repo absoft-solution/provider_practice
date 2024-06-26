63b47540507c00920ebf5c00ca63d657672599b9
Abdullah Anwar <info.ibnanwar@gmail.com>
2024-06-26 12:44:10 +0500

    Added Hide/Show feature in Stateless Widget

--- lib/screen/value_notify_listner.dart
+++ lib/screen/value_notify_listner.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class NotifyListenerScreen extends StatelessWidget {
   NotifyListenerScreen({super.key});
-  ValueNotifier _count = ValueNotifier<int>(0);
+  ValueNotifier<int> _count = ValueNotifier<int>(0);
+  ValueNotifier<bool> _isObscure = ValueNotifier<bool>(true);
   @override
   Widget build(BuildContext context) {
     print("build");
@@ -21,16 +22,43 @@ class NotifyListenerScreen extends StatelessWidget {
           color: const Color.fromRGBO(33, 150, 243, 1),
         ),
       ),
-      body: ValueListenableBuilder(
-          valueListenable: _count,
-          builder: (context, value, child) {
-            return Center(
-              child: Text(
-                _count.value.toString(),
-                style: TextStyle(fontSize: 50),
-              ),
-            );
-          }),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ValueListenableBuilder(
+                valueListenable: _isObscure,
+                builder: (context, value, child) {
+                  return TextField(
+                    obscureText: _isObscure.value,
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      suffix: InkWell(
+                          onTap: () {
+                            if (_isObscure.value == true) {
+                              _isObscure.value = false;
+                            } else {
+                              _isObscure.value = true;
+                            }
+                          },
+                          child: Icon(_isObscure.value
+                              ? Icons.visibility
+                              : Icons.visibility_off)),
+                    ),
+                  );
+                }),
+            ValueListenableBuilder(
+                valueListenable: _count,
+                builder: (context, value, child) {
+                  return Text(
+                    _count.value.toString(),
+                    style: TextStyle(fontSize: 50),
+                  );
+                }),
+          ],
+        ),
+      ),
     );
   }
 }
